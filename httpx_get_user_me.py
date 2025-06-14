17692ef64f76182ec2371d60948f29345cced133
offisnik <offisnik@gmail.com>
2025-06-14 21:50:54 +0500

    6.1 Введение в HTTPX. Практическое задание: работа с HTTPX

--- httpx_get_user_me.py
+++ httpx_get_user_me.py
@@ -4,48 +4,34 @@ url = "http://localhost:8000"
 
 # Сохраняем данные для входа в переменную
 email = input('Введите почту: ')
-password = input('Введите пароль: ')
 
 # Заготавливаем JSON данные для отправки в POST запросах
 reg_data = {
     "email": email,
-    "password": password,
+    "password": "default",
     "lastName": "default",
     "firstName": "default",
     "middleName": "default"
 }
 
 login_data = {
-    "email": email,
-    "password": password
+    "email": reg_data["email"],
+    "password": reg_data["password"]
 }
 
 
 # Регистрируем нового пользователя
-def reg_users():
-    httpx.post(f"{url}/api/v1/users", json=reg_data)
+httpx.post(f"{url}/api/v1/users", json=reg_data)
 
 
 # Выполняем authentication/login для пользователя
-def user_authentication():
-    login_response = httpx.post(f'{url}/api/v1/authentication/login', json=login_data)
+login_response = httpx.post(f'{url}/api/v1/authentication/login', json=login_data)
+accesstoken = login_response.json()['token']["accessToken"]
 
-    # Вытаскиваем токен и сохраняем его
-    accesstoken = login_response.json()['token']["accessToken"]
-    return accesstoken
+header = {
+    "Authorization": f"Bearer {accesstoken}"
+}
+check_response = httpx.get(f'{url}/api/v1/users/me', headers=header)
+print(check_response.json())
+print(check_response.status_code)
 
-
-# Проверяем что пользователь авторизован
-def check_user():
-    # Создаем Headers
-    accesstoken = {
-        "Authorization": f"Bearer {user_authentication()}"
-    }
-    check_response = httpx.get(f'{url}/api/v1/users/me', headers=accesstoken)
-    print(check_response.json())
-    print(check_response.status_code)
-
-
-reg_users()
-user_authentication()
-check_user()
