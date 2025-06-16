8f879062b7c48e303487fb31a288ea50fbda833e
offisnik <offisnik@gmail.com>
2025-06-16 14:08:25 +0500

    6.3 Знакомство с API клиентами. Практика: Написание API клиента

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,33 @@
+import httpx
+
+from typing import TypedDict
+
+from clients.api_client import APIClient
+
+
+class CreateRequestDict(TypedDict):
+    """
+    Описание запроса на регистрацию нового юзера
+    """
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+
+# Базовый URL для всех ендпоинтов
+# В APIClient не нашел где у нас записан BaseURL, поэтому передал сюда
+baseurl = 'http://localhost:8000'
+
+
+class PublicUsersClient(APIClient):
+    """
+    Клиент для работы с ендпоинтом /api/v1/users (метод POST)
+    """
+    def create_user_api(self, request: CreateRequestDict) -> httpx.Response:
+        """
+        :param request: Словарь с полями email, password, lastName, firstName, middleName
+        :return: Ответ от сервера в виде httpx.Response
+        """
+        return self.post(f"{baseurl}/api/v1/users", json=request)
